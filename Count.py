960e51abc856eb297f265ecd8348147ac46cb463
IsaacQ5 <Isaacquevy5@gmail.com>
2025-06-13 21:46:00 -0700

    "Made Count.py file"

---
+++ Count.py
@@ -0,0 +1,11 @@
+def count():
+    d = {}
+    with open("Names.txt", 'r') as f:
+        names = f.readlines()
+        for name in names:
+            name = name.strip()
+            if name in d:
+                d[name] += 1
+            else:
+                d[name] = 1
+count()
