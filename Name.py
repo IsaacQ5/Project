f46d1e5d85ac248a5e0f14cb807d54113f47101e
IsaacQ5 <Isaacquevy5@gmail.com>
2025-06-13 21:43:40 -0700

    "Made Names file"

--- Name.py
+++ Name.py
@@ -1,1 +1,13 @@
-print("work")
+import random
+def givename():
+    with open("Names.txt", 'w') as f:
+        name = ' '
+        for i in range(1000):
+            names = random.randint(1,2)
+            if names == 1:
+                name = "Isaac \n"
+            else:
+                name = "Carmen \n"
+            f.write(name)
+givename()
+
